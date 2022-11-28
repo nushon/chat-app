2c0e4d0102890c0b957d8d8fa6b13091e1f9f202
nushon <nushokarmo@gmail.com>
2022-11-28 15:30:17 +0000

    Chat app created and uers can chat in a group

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+// const { Socket } = require('socket.io');
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server (server);
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', (socket)=>{
+    socket.on('chat message', (msg) =>{
+        console.log("message: " + msg);
+    });
+    // console.log("a user is connected");
+    // socket.on('disconnect', ()=>{
+    //     console.log("a user has been disconnected")
+    // })
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+io.on('connection', (socket)=>{
+    socket.on('chat message', (msg)=>{
+        io.emit('chat message', msg);
+    })
+})
+server.listen(3000, ()=>{
+    console.log("Chat app is listening on port 3000")
+})
